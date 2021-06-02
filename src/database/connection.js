37aad31f731ec48d28e279d81a889bd761d6f7af
Ralac <andhyralac11@gmail.com>
2021-06-02 06:51:11 -0600

    Creando archivo y conexion a sql server usando variables de entorno

---
+++ src/database/connection.js
@@ -0,0 +1,21 @@
+import sql from 'mssql'
+
+const dbSettings = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
+    options: {
+        encrypt: true, // for azure
+        trustServerCertificate: true
+    }
+}
+
+export async function getConnection() {
+    try {
+        const pool = await sql.connect(dbSettings);
+        return pool;
+    } catch (error) {
+        console.log(error);
+    }
+}
