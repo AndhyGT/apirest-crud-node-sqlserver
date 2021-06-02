bce19053e9f2fb9db314e7b1356545b0c64277c4
Ralac <andhyralac11@gmail.com>
2021-06-02 06:54:49 -0600

    Agregando las rutas de productos al archivo app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import config from './config'
 
+import productRoutes from './router/products.routes'
+
 const app = express();
 
 
 // settings
 app.set('port', config.port)
-
+app.use(productRoutes)
 
 export default app;
